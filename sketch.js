98dd12d66df4899bde0091aff9b10fe8e52457d5
adhityakannan25 <67790888+adhityakannan25@users.noreply.github.com>
2020-08-11 19:57:18 +0530

    Added changing background with reference to real time.

--- sketch.js
+++ sketch.js
@@ -1,18 +1,19 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, pig1;
-var backgroundImg;
-var ground2;
-var sling;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
 
-var gameState = "onSling"
-
+var gameState = "onSling";
+var bg = "sprites/bg1.png";
+var score = 0;
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImg();
 }
 
 function setup(){
@@ -20,9 +21,9 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+
     ground = new Ground(600,height,1200,20);
-    ground2 = new Ground(200,300,400,200);
+    platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -39,29 +40,34 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
-    //log6=new Log(200,100,100,PI);
-    sling = new Slingshot(bird.body,{x:200,y:50});
-
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new Slingshot(bird.body,{x:200, y:50});
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg)
+        background(backgroundImg);
+    
+        noStroke();
+        textSize(35)
+        fill("white")
+        text("Score  " + score, width-300, 50)
+    
     Engine.update(engine);
-    /*console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);*/
+    //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
-    ground2.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -69,23 +75,43 @@ function draw(){
     log5.display();
 
     bird.display();
-
-    sling.display();
-
+    platform.display();
+    //log6.display();
+    slingshot.display();    
 }
 
 function mouseDragged(){
-    if(gameState!="launched")
-    Matter.Body.setPosition(bird.body, {
-    x: mouseX, y: mouseY
-})
+    if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
+
+
 function mouseReleased(){
- gameState="launched"
- sling.fly()
+    slingshot.fly();
+    gameState = "launched";
 }
+
 function keyPressed(){
-if(keyCode===32){
-  sling.attach(bird.body)
- }
+    if(keyCode === 32){
+       // slingshot.attach(bird.body);
+    }
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=06 && hour<=19){
+        bg = "sprites/bg1.png";
+    }
+    else{
+        bg = "sprites/bg2.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
 }
